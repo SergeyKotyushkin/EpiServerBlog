d34c385c5a4142f69b3d1c34946e58fcd9998e2e
SergeyKotyushkin <sergey.kotyushkin@firstlinesoftware.com>
2016-10-26 16:13:32 +0300

    Add blogs list to Start Page

---
+++ EpiServerBlogs.Web/Models/Blocks/BlogBlock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EpiServerBlogs.Web.Models.Blocks
+{
+    [ContentType(DisplayName = "BlogBlock", GUID = "9ab22edd-5d5d-4445-b890-3faa18c87a43", Description = "")]
+    public class BlogBlock : BlockData
+    {
+        [Display(
+            Name = "Blog Header",
+            Description = "The header of the new blog",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual string BlogHeader { get; set; }
+
+        [Display(
+            Name = "Blog Author",
+            Description = "The author of the new blog",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual string BlogAuthor { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Articles Content Area",
+            Description = "It allows you to add new articles into the current blog",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual ContentArea ArticlesContentArea { get; set; }
+    }
+}
--- EpiServerBlogs.Web/Models/Pages/StartPage.cs
+++ EpiServerBlogs.Web/Models/Pages/StartPage.cs
@@ -1,10 +1,19 @@
-﻿using EPiServer.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
 
 namespace EpiServerBlogs.Web.Models.Pages
 {
     [ContentType(DisplayName = "StartPage", GUID = "82353600-82f4-4965-bc27-94a2866433a2", Description = "")]
     public class StartPage : SitePageData
     {
-        
+        [CultureSpecific]
+        [Display(
+            Name = "Blog Content Area",
+            Description = "It allows you to add new blog",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual ContentArea BlogBlockContentArea { get; set; }
     }
 }
